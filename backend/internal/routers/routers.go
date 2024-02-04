f570b2b4aa12e6bf3047efd0be11440f1e53b805
nyrta1 <nurtai_2005@mail.ru>
2024-02-05 04:57:27 +0600

    feat: added tag to router

--- backend/internal/routers/routers.go
+++ backend/internal/routers/routers.go
@@ -9,12 +9,14 @@ import (
 type Routers struct {
 	authHandlers handlers.AuthHandlers
 	newsHandlers handlers.NewsHandlers
+	tagHandlers  handlers.TagHandlers
 }
 
-func NewRouters(authHandlers handlers.AuthHandlers, newsHandlers handlers.NewsHandlers) *Routers {
+func NewRouters(authHandlers handlers.AuthHandlers, newsHandlers handlers.NewsHandlers, tagHandlers handlers.TagHandlers) *Routers {
 	return &Routers{
 		authHandlers: authHandlers,
 		newsHandlers: newsHandlers,
+		tagHandlers:  tagHandlers,
 	}
 }
 
@@ -33,9 +35,15 @@ func (r *Routers) SetupRoutes(app *gin.Engine) {
 			newsRouter.POST("/create", middleware.RequireAuthMiddleware, r.newsHandlers.CreateNews)
 			newsRouter.GET("/getByID", r.newsHandlers.GetNewsByObjectID)
 			newsRouter.GET("/getByAuthor", r.newsHandlers.GetAllNewsByAuthor)
-			newsRouter.GET("/getByTag", r.newsHandlers.GetAllNewsByTag)
 			newsRouter.PUT("/updateByID", middleware.RequireAuthMiddleware, r.newsHandlers.UpdateNewsByObjectID)
 			newsRouter.DELETE("/deleteByID", middleware.RequireAuthMiddleware, r.newsHandlers.DeleteNewsByObjectID)
+		}
+		tagRouter := appRouter.Group("/tag")
+		{
+			tagRouter.POST("/add", middleware.RequireAuthMiddleware, r.tagHandlers.AddTagToNewsByNewsID)
+			tagRouter.GET("/get", r.tagHandlers.GetTagsByNewsID)
+			tagRouter.PUT("/update", middleware.RequireAuthMiddleware, r.tagHandlers.UpdateTagByNewsID)
+			tagRouter.DELETE("/delete", middleware.RequireAuthMiddleware, r.tagHandlers.DeleteTagByNewsID)
 		}
 	}
 }
