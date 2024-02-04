80fd015d81a0e60240f28b15b80447f9441ed0db
nyrta1 <nurtai_2005@mail.ru>
2024-02-05 02:28:50 +0600

    feat: add news routers

--- backend/internal/routers/routers.go
+++ backend/internal/routers/routers.go
@@ -8,11 +8,13 @@ import (
 
 type Routers struct {
 	authHandlers handlers.AuthHandlers
+	newsHandlers handlers.NewsHandlers
 }
 
-func NewRouters(authHandlers handlers.AuthHandlers) *Routers {
+func NewRouters(authHandlers handlers.AuthHandlers, newsHandlers handlers.NewsHandlers) *Routers {
 	return &Routers{
 		authHandlers: authHandlers,
+		newsHandlers: newsHandlers,
 	}
 }
 
@@ -25,6 +27,15 @@ func (r *Routers) SetupRoutes(app *gin.Engine) {
 			authRouter.POST("/login", r.authHandlers.Login)
 			authRouter.POST("/logout", middleware.RequireAuthMiddleware, r.authHandlers.Logout)
 			authRouter.DELETE("/deleteAccount", middleware.RequireAuthMiddleware, r.authHandlers.DeleteAccount)
+		}
+		newsRouter := appRouter.Group("/news")
+		{
+			newsRouter.POST("/create", middleware.RequireAuthMiddleware, r.newsHandlers.CreateNews)
+			newsRouter.GET("/getByID", r.newsHandlers.GetNewsByObjectID)
+			newsRouter.GET("/getByAuthor", r.newsHandlers.GetAllNewsByAuthor)
+			newsRouter.GET("/getByTag", r.newsHandlers.GetAllNewsByTag)
+			newsRouter.PUT("/updateByID", middleware.RequireAuthMiddleware, r.newsHandlers.UpdateNewsByObjectID)
+			newsRouter.DELETE("/deleteByID", middleware.RequireAuthMiddleware, r.newsHandlers.DeleteNewsByObjectID)
 		}
 	}
 }
