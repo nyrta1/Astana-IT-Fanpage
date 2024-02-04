97ff34cff3ca861c5dd75a4c45e872fea1eb1a75
nyrta1 <nurtai_2005@mail.ru>
2024-02-05 02:26:35 +0600

    fix: remove objectID from News

--- backend/internal/rest/models/models.go
+++ backend/internal/rest/models/models.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"time"
+)
+
+// PostgresSQL Models
 
 type User struct {
 	ID        uint      `json:"ID,omitempty"`
@@ -8,4 +12,25 @@ type User struct {
 	Username  string    `json:"username" json:"Username,omitempty"`
 	Password  string    `json:"password" json:"Password,omitempty"`
 	UserType  uint      `json:"userType"`
+}
+
+// MongoDB Models
+
+type News struct {
+	Tags      []Tag     `json:"tags" bson:"tags"`
+	Author    string    `json:"author" bson:"author"`
+	Content   string    `json:"content" bson:"content"`
+	CreatedAt time.Time `json:"created_at" bson:"created_at"`
+	Comments  []Comment `json:"comments" bson:"comments"`
+}
+
+type Comment struct {
+	Username  string    `json:"user" bson:"user"`
+	Content   string    `json:"content" bson:"content"`
+	CreatedAt time.Time `json:"created_at" bson:"created_at"`
+}
+
+type Tag struct {
+	Tag       string    `json:"tag" bson:"tag"`
+	CreatedAt time.Time `json:"created_at" bson:"created_at"`
 }
