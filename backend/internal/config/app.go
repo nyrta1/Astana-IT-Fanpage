ee8b5fbd84a3f0b47e59409d5bb090012abd5ab3
nyrta1 <nurtai_2005@mail.ru>
2024-02-04 22:42:19 +0600

    feat: mongodb config added to app config

--- backend/internal/config/app.go
+++ backend/internal/config/app.go
@@ -1,7 +1,8 @@
 package config
 
 type App struct {
-	PORT  string `env:"APP_PORT" envDefault:"8080"`
-	DB    Database
-	Redis RedisConfig
+	PORT          string `env:"APP_PORT" envDefault:"8080"`
+	DB            Database
+	Redis         RedisConfig
+	MongoDbConfig MongoDbConfig
 }
